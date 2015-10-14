a0842cf6d01e0beeb426b7f8c38611ec4d5f1a2b
Nazarett <gnazarett@gmail.com>
2015-10-14 00:21:42 -0400

    starting copying code for project

--- Nazarett_Gerardo_Project3/Resources/app.js
+++ Nazarett_Gerardo_Project3/Resources/app.js
@@ -1,28 +1,46 @@
-//Practice for video week 3 
-var win = Ti.UI.createWindow({
-	backgroundColor: "#fff",
-	layout:"vertical"
+var pWidth = Ti.Platform.displayCaps.platformWidth;
+var pHeight = Ti.Platform.displayCaps.platformWidth;
+var itemCount = 15;
+var rowCount = 3;
+var margin = 10;
+var trueCanvasWidth = ;
+var size = ;
+
+var mainWin = Ti.UI.createWindow({
+	backgroundColor: "#fff"
+	//layout: "horizontal"
 });
 
-//var myPic = Ti.UI.createImageView({
-	//image: "pics/scorpion.jpg",
-	//width: "70%"
-//});
+var border = Ti.UI.createView({
+	backgroundColor: "orange",
+	heght: 1,
+	width: pWidth,
+	top: 20
+});
 
+var viewContainer = Ti.UI.createView({
+	//top: 0
+	width: pWidth,
+	backgroundColor: "fef"
+});
 
-//var myPics = ["scorpion.jpg", "oreo.jpg"];
-
-var fotos = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory,"pics" );
-var myPics = fotos.getDirectoryListing();
-
-for(var i=0; i<myPics.length;i++){
-	var thePic = Ti.UI.createImageView({
-		image: "pics/" + myPics [i],
-		width : "50%",
-		
+for( i = 0; i < itemCount; i++){
+	var view = Ti.UI.createView({
+		backgroundColor: "#33CCFF",
+		top: margin,
+		left: margin,
+		width: size,
+		height: size
 	});
-	win.add(thePic);
+	var text = Ti.UI.createLabel({
+		text: i+1,
+		color:"#fff"
+	});
+	
+	view.add(text);
+	viewContainer.add(view);
 }
 
-win.add(myPics);
-win.open();
+mainWin.add(border);
+mainWin.add(viewContainer);
+mainWin.open();
